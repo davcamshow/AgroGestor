43352b5524002f7b041b9b4764e5d8b99100ebc1
davcamshow <davidbarrerareyes98@gmail.com>
2026-02-18 22:18:56 -0600

    django

---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(), 
+    tailwindcss()
+  ],
+build: {
+    outDir: '../backend/static',  // Construir directamente en static de Django
+    emptyOutDir: true,
+  },
+  base: '/static/',  // Importante para que Django encuentre los archivos
+  server: {
+    port: 5173,
+    strictPort: true,
+  }
+})
