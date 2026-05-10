649df4f8671ed6dc294ce7929a1a8b8ec7044619
Maria <ortizmerinomaria7@gmail.com>
2026-05-10 10:57:30 -0600

    mary tktk

--- mobile/lib/core/api/api_client.dart
+++ mobile/lib/core/api/api_client.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../auth/token_storage.dart';
 
 // ⚠️ CAMBIAR ESTA IP AL CONECTAR A DIFERENTE RED
-const String _baseUrl = 'http://192.168.0.104:8000/api/';
+const String _baseUrl = 'http:// 192.168.0.8:8000/api/';
 
 final apiClientProvider = Provider<ApiClient>((ref) {
   final tokenStorage = ref.read(tokenStorageProvider);
